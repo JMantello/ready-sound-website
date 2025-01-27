4318d2ec2261fea993c327ad65c5f64ef893ef58
Jonathan Mantello <38162257+JMantello@users.noreply.github.com>
2025-01-26 23:32:39 -0800

    add footer and quote sections

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from './components/Navbar'
 import Packages from './components/Packages'
 import ValueProps from './components/ValueProps'
 import LiveSoundEngineering from './components/LiveSoundEngineering'
+import RequestAQuote from './components/RequestAQuote'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
       <ValueProps />
       <Packages />
       <LiveSoundEngineering />
+      <RequestAQuote />
+      <Footer />
     </>
   )
 }
---
+++ src/components/RequestAQuote.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/RequestAQuote.css';
+
+const RequestAQuote = () => {
+  return (
+    <div className="request-a-quote">
+      <div className="hero-image">
+        <div className="hero-text">
+          <h1>Request A Quote</h1>
+          <p>Get the best sound solutions for your needs</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RequestAQuote;
