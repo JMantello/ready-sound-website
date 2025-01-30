5a086709a1d74569a1e061e82dcfebf25b20b3dd
Jonathan Mantello <38162257+JMantello@users.noreply.github.com>
2025-01-29 18:51:48 -0800

    update requeset a quote image

--- src/components/LiveSoundEngineering/LiveSoundEngineering.jsx
+++ src/components/LiveSoundEngineering/LiveSoundEngineering.jsx
@@ -4,15 +4,20 @@ import './LiveSoundEngineering.css';
 const LiveSoundEngineering = () => {
   return (
     <section className="live-sound-engineering">
-      <h2>Live Sound Engineering</h2>
-      <h3>Make Your Live Event Easy with Professional Audio Support</h3>
-      <p>Let our experienced audio engineers handle your event, so everything runs smoothly and sounds perfect. Whether it’s a small event or a full production, we’re here to make it happen.</p>
-      <ul className="rates">
-        <li>$250 for a half day (up to 4 hours)</li>
-        <li>$400 for a full day (up to 8 hours)</li>
-        <li>$50 per additional hour</li>
-      </ul>
-      <a className='button' href='/contact'>Book an Engineer</a>
+      <div className='left'>
+        <img src='/images/sound-live.jpg' alt='Live Sound Engineering' />
+      </div>
+      <div className='right'>
+        <h2>Live Sound Engineering</h2>
+        <h3>Make Your Live Event Easy with Professional Audio Support</h3>
+        <p>Let our experienced audio engineers handle your event, so everything runs smoothly and sounds perfect. Whether it’s a small event or a full production, we’re here to make it happen.</p>
+        <ul className="rates">
+          <li>$250 for a half day (up to 4 hours)</li>
+          <li>$400 for a full day (up to 8 hours)</li>
+          <li>$50 per additional hour</li>
+        </ul>
+        <a className='button' href='/contact'>Book an Engineer</a>
+      </div>
     </section>
   );
 };
--- src/components/RequestAQuote/RequestAQuote.jsx
+++ src/components/RequestAQuote/RequestAQuote.jsx
@@ -5,10 +5,12 @@ const RequestAQuote = () => {
   return (
     <div className="request-a-quote">
       <div className="hero-image">
-        <div className="hero-text">
-          <h1>Request A Quote Today</h1>
-          <p>Get the best sound solutions for your needs</p>
-          <a className='button' >Get A Quote</a>
+        <div className="hero-overlay">
+          <div className="hero-text">
+            <h1>Request A Quote Today</h1>
+            <p>Get a fast, easy, and personalized quote tailored to your needs.</p>
+            <a className="button">Get A Quote</a>
+          </div>
         </div>
       </div>
     </div>
